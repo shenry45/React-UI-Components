ec91903d1bbf4078953471e16e28ad3d19ea3e24
shenry45 <shawn45henry@gmail.com>
2019-05-08 20:04:59 -0700

    CSS for footer of social-card

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,23 +3,22 @@ import './Footer.css';
 
 const FooterContainer = () => {
   return (
-    <footer>
+    <div className="post-footer">
       <div>
         <i className="far fa-comment"></i>
       </div>
       <div>
         <i className="fas fa-sync"></i>
-        <p>1</p>
+        <p>6</p>
       </div>
       <div>
         <i className="far fa-heart"></i>
-        <p>1</p>
+        <p>4</p>
       </div>
       <div>
         <i className="far fa-envelope"></i>
-        <p>1</p>
       </div>
-    </footer>
+    </div>
   );
 };
 
